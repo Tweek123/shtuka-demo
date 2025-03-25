677478281b911e21ea300d91ce766e9f211b4de5
Tweek <tweek1996@gmail.com>
2025-03-25 22:48:05 +0300

    update

--- src/components/Video.tsx
+++
@@ -1,7 +0,0 @@
-const Video = ({src, className}: {src: string, className?: string}) => {
-    return <div className={`flex flex-col aspect-video ${className}`}>
-    <iframe width="100%" height="100%" src={src} frameBorder="0" allow="clipboard-write; autoplay" allowFullScreen></iframe>
-    </div>
-}
-
-export default Video
